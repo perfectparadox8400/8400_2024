73dc2dec0066a1e10fd0203388f3b3e2ad26319b
FTC Team Perfect Paradox 8400 <83031328+ftc8400@users.noreply.github.com>
2024-01-12 16:40:18 -0700

    THE ARM WORKSSSSS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive/opmode/HoldPosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive/opmode/HoldPosition.java
@@ -5,14 +5,17 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import static java.lang.Math.abs;
 import static java.lang.Math.cos;
 import static java.lang.Math.toRadians;
 
-@TeleOp(name = "Hold Arm Position")
+@TeleOp(name = "!Hold Arm Position")
 public class HoldPosition extends LinearOpMode {
     private DcMotor mainBoom1;
     private DcMotor mainBoom2;
     private DcMotor jibBoom;
+    private Servo elbow;
+    private Servo hand;
 
 
 
@@ -25,12 +28,15 @@ public class HoldPosition extends LinearOpMode {
         mainBoom1 = hardwareMap.get(DcMotor.class, "arm1");
         mainBoom2 = hardwareMap.get(DcMotor.class, "arm2");
         jibBoom = hardwareMap.get(DcMotor.class, "arm3");
+        elbow = hardwareMap.get(Servo.class, "elbow");
+        hand = hardwareMap.get(Servo.class, "hand");
         mainBoom1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         mainBoom2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         jibBoom.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         mainBoom1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         mainBoom2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         jibBoom.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
 
         double ticks = 282;
 
@@ -39,41 +45,112 @@ public class HoldPosition extends LinearOpMode {
 
         double c1 = -.45;
         double c2 = -.2;
-        double c3 = .2;
+        double c3 = .1;
 
+        double c1Tmp;
+        double c2Tmp;
+        double c3Tmp;
+ 
         double Pa = 0;
         double Pb = 0;
 
+        double elbowTarget = 0;
+
+        double powerMod;
+
         boolean up_pressed = true;
         boolean down_pressed = true;
+
+
 
         waitForStart();
         if (opModeIsActive()) {
             while (opModeIsActive()) {
                 angle1 = mainBoom1.getCurrentPosition() * 360/ticks;
                 angle2 = jibBoom.getCurrentPosition() * 360/ticks;
+                angle1 += 18;
+                angle2 -= 180;
 
-                //This is for debugging - delete later.
-                if (gamepad2.dpad_up && up_pressed) {
-                    c2 -= .01;
-                    up_pressed = false;
-                } else if (!gamepad2.dpad_up){
-                    up_pressed = true;
-                }
-
-                if (gamepad2.dpad_down && down_pressed) {
-                    c2 += .01;
-                    down_pressed = false;
-                } else if (!gamepad2.dpad_down) {
+                if (gamepad2.dpad_left && (elbowTarget < 1)) {
+                    if (down_pressed) {
+                        elbowTarget += .05;
+                        down_pressed = false;
+                    }
+                } else {
                     down_pressed = true;
                 }
 
+                if (gamepad2.dpad_right && (elbowTarget > 0)) {
+                    if (up_pressed) {
+                        elbowTarget -= .05;
+                        up_pressed = false;
+                    }
+                } else {
+                    up_pressed = true;
+                }
+
+                elbow.setPosition(elbowTarget);
+
+                c1Tmp = c1;
+                c2Tmp = c2;
+                c3Tmp = c3;
+
+                //Reset Encoders
+                if (gamepad2.start) {
+                    mainBoom1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    mainBoom2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    jibBoom.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    mainBoom1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    mainBoom2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    jibBoom.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+                }
+
+                //Kill Main Boom Motors
+                if (gamepad2.left_stick_button) {
+                    c1Tmp = 0;
+                    c2Tmp = 0;
+                }
+                //Kill Jib Boom Motors
+                if (gamepad2.right_stick_button) {
+                    c3Tmp = 0;
+                }
 
 
-                Pa = (c1 * cos(toRadians(angle1))) + (c2*cos(toRadians(angle1 + angle2)));
-                Pb = (c3 * cos(toRadians(angle1+angle2)));
+
+
+                Pa = (c1Tmp * cos(toRadians(angle1))) + (c2Tmp*cos(toRadians(angle1 + abs(angle2))));
+                Pb = (c3Tmp * cos(toRadians(abs(angle1)+angle2)));
                 //Pa = c1+c2;
                 //Pb = c3;
+
+
+
+
+                if (gamepad2.left_bumper) {
+                    powerMod = .5;
+                } else if (gamepad2.right_bumper) {
+                    powerMod = 2;
+                } else {
+                    powerMod = 1;
+                }
+
+                if (gamepad2.dpad_up) {
+                    Pa += .3;
+                    Pa *= powerMod;
+                } else if (gamepad2.dpad_down) {
+                    Pa -= .3;
+                    Pa *= powerMod;
+                }
+                if (gamepad2.y) {
+                    Pb += .3;
+                    Pb *= powerMod;
+                } else if (gamepad2.a) {
+                    Pb -= .3;
+                    Pb *= powerMod;
+                }
+
+
 
                 mainBoom1.setPower(Pa);
                 mainBoom2.setPower(Pa);
@@ -88,8 +165,8 @@ public class HoldPosition extends LinearOpMode {
                 telemetry.addData("C 2 Var", c2);
                 telemetry.addData("Main Boom Angle", angle1);
                 telemetry.addData("Jib Boom Angle", angle2);
-                telemetry.addData("AnglesAdded", angle1 + angle2);
-
+                telemetry.addData("AnglesAdded", abs(angle1) + angle2);
+                telemetry.addData("Servo Target Position", elbowTarget);
                 telemetry.update();
             }
         }
